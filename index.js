3f154065f4330dacd097bc8b7a6c2613acdf5926
anshul <ansulsingh171@gamil.com>
2025-03-22 16:38:21 +0530

    add index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ let heading=document.getElementsByTagName('h1')[0];
 heading.style.color='red';
 heading.style.fontSize='50px';
 heading.style.fontFamily='Arial';
+heading.style.textAlign='center';
+heading.style.backgroundColor='yellow';
+heading.style.padding='20px';
+
+// this is master branch
+// this is master branch
